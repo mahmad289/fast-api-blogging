e27970acd8341d94f9f432935d7f97d5228cf7ff
Muhammad Ahmad <muhammad.ahmad@formsoft-inc.com>
2024-07-28 12:22:00 +0500

    User creation and password hashing

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from . import schemas, models
+from . import schemas, models, hashing
 from .database import engine, SessionLocal
 
 from sqlalchemy.orm import Session
@@ -55,3 +55,18 @@ def show(id,response:Response, db: Session= Depends(get_db)):
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
     return blog
+
+@app.post('/user', response_model=schemas.ShowUser)
+def create_user(request:schemas.User, db: Session= Depends(get_db)):
+    new_user = models.User(name=request.name, email= request.email, password= hashing.Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/user/{id}', status_code=200,response_model=schemas.ShowUser)
+def get_user(id:int, db: Session= Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with the id {id} not found')
+    return user
